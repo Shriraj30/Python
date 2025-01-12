ebbbbc384f563eee0000b55b57983b192310f156
shriraj Dharmadhikari <145005979+Shriraj30@users.noreply.github.com>
2025-01-12 14:16:30 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace urbanreg.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
